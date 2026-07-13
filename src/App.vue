054de12cff7edd237881163f84fd6c3168387bd9
Uchechukwu-best <uchman6122@gmail.com>
2026-07-13 11:49:40 +0100

    Fetching jobs from json file and removing hard coded values.

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Narbar from "./components/Narbar.vue";
 import Hero from "./components/Hero.vue";
 import HomeCards from "./components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
 </script>
 
 <template>
   <Narbar />
   <Hero />
   <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
