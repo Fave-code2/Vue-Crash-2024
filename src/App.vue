e8f6f6eda6891052b11d29244c48b06e0126c65c
Uchechukwu-best <uchman6122@gmail.com>
2026-07-13 09:45:32 +0100

    Hero and Card components added.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import Narbar from "./components/Narbar.vue";
+import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
 </script>
 
 <template>
   <Narbar />
+  <Hero />
+  <HomeCards />
 </template>
